b0720b6901f07e5bba5ecb1e450f80d64c5b017a
Marcel <marcel.mikolas@gmail.com>
2021-07-05 13:34:09 +0200

    App

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const app = express();
 const PORT = 6969;
-
+app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
+
 app.get("/", (req, res) => {
     var ip = req.headers['x-forwarded-for']
     if (!ip) {
         ip = "Vaší ip adresu nebylo možné získat :("
     }
-    console.log(ip);
     res.render('index.ejs', { ip: ip });
 });
 
